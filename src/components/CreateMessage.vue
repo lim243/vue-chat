dee3d469e8e9523613d791056497b249f61c9ed7
Andrew T. Lim <lim243@purdue.edu>
2021-10-12 21:45:02 -0400

    Add emoji picker

--- src/components/CreateMessage.vue
+++ src/components/CreateMessage.vue
@@ -1,50 +1,79 @@
 <template>
   <div class="container">
-    <form @submit.prevent="createMessage">
-      <div class="form-group">
-        <input
-          type="text"
-          class="form-control"
-          name="message"
-          placeholder="Enter something here..."
-          v-model="newMessage"
-        />
-        <p class="text-danger" v-if="errorText">{{ errorText }}</p>
+    <form class="inline" @submit.prevent="createMessage">
+      <div class="form-group row">
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control"
+            name="message"
+            placeholder="Enter something here..."
+            v-model="newMessage"
+          />
+        </div>
+        <div class="col-sm-1">
+          <DiscordPicker
+            :showEmoji="showEmoji"
+            :apiKey="apiKey"
+            @emoji="setEmoji"
+          />
+        </div>
+
+        <div class="col-sm-1">
+          <button class="btn btn-primary" type="submit" name="action">
+            Send
+          </button>
+        </div>
       </div>
-      <button class="btn btn-primary" type="submit" name="action">Send</button>
+      <p class="text-danger" v-if="errorText">{{ errorText }}</p>
     </form>
   </div>
 </template>
 
 <script>
 import { db, collection, addDoc } from "@/firebase/init";
+import DiscordPicker from "vue3-discordpicker";
 
 export default {
   name: "CreateMessage",
   props: ["name"],
+  components: {
+    DiscordPicker,
+  },
   data() {
     return {
-      newMessage: null,
+      newMessage: "",
       errorText: null,
+      showEmoji: true,
+      emojiValue: null,
+      apiKey: process.env.VUE_APP_EMOJI_API_KEY,
     };
   },
   methods: {
     createMessage() {
-      if (this.newMessage) {
+      if (this.newMessage.length > 0) {
         const docData = {
           message: this.newMessage,
           name: this.name,
           timestamp: Date.now(),
         };
-        
+
         addDoc(collection(db, "messages"), docData);
 
         // Reset message form
-        this.newMessage = null;
+        this.newMessage = "";
         this.errorText = null;
       } else {
         this.errorText = "Message cannot be empty! Please enter something...";
       }
+    },
+    setEmoji(emoji) {
+      console.log(emoji);
+      this.newMessage += emoji;
+    },
+    setGif(gif) {
+      console.log(gif);
+      this.newMessage += " " + gif;
     },
   },
 };
--- src/firebase/init.js
+++ src/firebase/init.js
@@ -10,10 +10,96 @@ import {
   setDoc,
   addDoc,
 } from "firebase/firestore";
+import {
+  getAuth,
+  sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+} from "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
 
 initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-export { db, collection, query, where, onSnapshot, orderBy, doc, setDoc, addDoc };
+const auth = getAuth();
+
+const actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: "http://localhost:8080/chat",
+  // This must be true.
+  handleCodeInApp: true,
+};
+
+const emailAuth = (email) => {
+  sendSignInLinkToEmail(auth, email, actionCodeSettings)
+    .then(() => {
+      // The link was successfully sent. Inform the user.
+      // Save the email locally so you don't need to ask the user for it again
+      // if they open the link on the same device.
+      window.localStorage.setItem("emailForSignIn", email);
+      console.log("email sent to:", email);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("errorCode, errorMessage", errorCode, errorMessage);
+      // ...
+    });
+};
+
+const checkAuth = () => {
+  console.log("in checkAuth", window.location.href);
+  // Confirm the link is a sign-in with email link.
+  if (isSignInWithEmailLink(auth, window.location.href)) {
+    console.log("hi");
+    // Additional state parameters can also be passed via URL.
+    // This can be used to continue the user's intended action before triggering
+    // the sign-in operation.
+    // Get the email if available. This should be available if the user completes
+    // the flow on the same device where they started it.
+    let email = window.localStorage.getItem("emailForSignIn");
+    if (!email) {
+      // User opened the link on a different device. To prevent session fixation
+      // attacks, ask the user to provide the associated email again. For example:
+      email = window.prompt("Please provide your email for confirmation");
+    }
+
+    console.log("email", email);
+
+    // The client SDK will parse the code from the link for you.
+    signInWithEmailLink(auth, email, window.location.href)
+      .then((result) => {
+        // Clear email from storage.
+        window.localStorage.removeItem("emailForSignIn");
+        console.log("result", result);
+        // You can access the new user via result.user
+        // Additional user info profile not available via:
+        // result.additionalUserInfo.profile == null
+        // You can check if the user is new or existing:
+        // result.additionalUserInfo.isNewUser
+      })
+      .catch((error) => {
+        // Some error occurred, you can inspect the code: error.code
+        // Common errors could be invalid email and invalid or expired OTPs.
+        console.log("error", error);
+      });
+  }
+};
+
+export {
+  auth,
+  emailAuth,
+  checkAuth,
+  db,
+  collection,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+  doc,
+  setDoc,
+  addDoc,
+};
